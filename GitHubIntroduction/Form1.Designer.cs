ccea4b44dd7cb484db5d6cdf0e0ad7f7b6156d85
dimitrijekokovic <123652853+dimitrijekokovic@users.noreply.github.com>
2023-01-26 17:17:26 +0100

    Gle

--- GitHubIntroduction/Form1.Designer.cs
+++ GitHubIntroduction/Form1.Designer.cs
@@ -65,11 +65,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(149, 99);
+            this.button2.Location = new System.Drawing.Point(62, 98);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "taralala";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
